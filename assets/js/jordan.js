f3528acb81778df4b26bff52486b10b4a34a7bb4
Yong Lim <yong.lim@gmail.com>
2024-11-27 10:58:16 +0700

    Use Google Forms and spreadsheet combo for contact us

--- assets/js/jordan.js
+++ assets/js/jordan.js
@@ -3,12 +3,72 @@ $(function() {
 });
 
 function formFocus() {
-  $('#alert-field')
-    .removeClass()
-    .addClass('hidden');
+  $('#alert-field').hide();
 }
 
-function sendGreenhouse(e) {
+function formReset() {
+  $('#alert-field').show()
+    .html("<span><p>Thank you. We'll get back to you asap.</p></span><br>");
+  document.getElementById("formID").reset();
+  setTimeout(() => {
+    $('#sendButton').show();
+    $('#alert-field').hide();
+    console.log('This alert appeared after 3 second!'); 
+  }, 3000);
+}
+
+function pleaseWaite() {
+  $('#sendButton').hide();
+  $('#alert-field').removeClass();
+  $('#alert-field').show()
+    .html("<span><p>Please wait while we're sending your message . . .</p>  <progress></progress></span>");
+}
+
+//selector from your HTML form
+function postGreenhouse(e) {
+  //prevent the form from submiting so we can post to the google form
+  console.log("in postGreenhouse");
+  e.preventDefault();
+  // console.log("inpostEatery");
+  pleaseWaite();
+
+  //AJAX request
+  $.ajax({
+    //The public Google Form url, but replace /view with /formResponse
+    url: 'https://docs.google.com/forms/d/e/1FAIpQLSd-0eFGpF8gqkf4EOx4wrGsyy0yiVZIV0sO4HmK03uYq-ol5g/formResponse',     
+    data: $('#formID').serialize(), //Nifty jquery function that gets all the input data 
+    type: 'POST', //tells ajax to post the data to the url
+    dataType: "json", //the standard data type for most ajax requests
+    mode: 'cors',
+    statusCode: { //the status code from the POST request
+      0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
+        //success
+        formReset();
+       }, 
+       200: function(data) {//200 is a success code. it went through!
+        //success
+        // $('#form-success').text('hooray! 200');
+        formReset();
+       },
+       403: function(data) {//403 is when something went wrong and the submission didn't go through
+        //error
+        $('#alert-field').show()
+          .html("<span><p><b>Oh no! something went wrong. Please let us know of your problem.</b></p></span>");
+        alert('Oh no! something went wrong. Please let us know of your problem.');
+      }
+    },
+    statusText: { //the status code from the POST request
+      "error": function(data) {
+        //error
+        $('#alert-field').show()
+          .html("<span><p><b>Oh no! something went wrong. Please let us know of your problem.</b></p></span>");
+        alert('Oh no! something went wrong. Please let us know of your problem.');
+      }
+    }
+  });
+}
+
+function delete_sendGreenhouse(e) {
   e.preventDefault();
 
   console.log("in sendGreenhouse");
@@ -35,6 +95,8 @@ function sendGreenhouse(e) {
     $.post(POST_URL, JSON.stringify(postRequest))
       .then(res => {
         e.target.reset();
+        console.log("res.code: ", res.code);
+        console.log("res.msg: ", res.msg);
         $('#alert-field')
           .removeClass()
           .addClass(`alert alert-${res.code}`)
